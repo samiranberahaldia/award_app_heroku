4bd2d5fb4e0b33a8580daeb32d3a78e3c4c66f6b
Samiran Bera <49357196+samiranberahaldia@users.noreply.github.com>
2020-06-28 20:34:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ lrs = pickle.load(open('lr_model.pkl', 'rb'))
 def home():
     return render_template('award_index.html')
 
-@app.route('/predict', methods = ['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
-    features = np.array([[int(x) for x in request.forms.values()]])
-    #features = [np.array(features)]
-    y_prediction = lrs.predict(features)[0]  
-    return render_template('award_index.html', prediction_text='Employee Salary should be $ {}'.format(y_prediction))   
+    features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    
+    return render_template('award_index.html', prediction_text='Employee Salary should be $ {}'.format(prediction[0]))   
 
 if __name__=="__main__":
     app.run(debug=True)
